6c750c607314f34ac40e526e6a8886460e7ec3d0
Yuto Tokunaga <yuntan.sub1@gmail.com>
2017-06-24 19:22:02 +0900

    ScreenshotObserverService startForeground
    Show notification for startForeground

--- app/src/main/java/xyz/untan/gyazauto/ScreenshotObserverService.java
+++ app/src/main/java/xyz/untan/gyazauto/ScreenshotObserverService.java
@@ -1,5 +1,7 @@
 package xyz.untan.gyazauto;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.database.ContentObserver;
@@ -7,12 +9,16 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 public class ScreenshotObserverService extends Service {
     static final String TAG = ScreenshotObserverService.class.getSimpleName();
     //    private FileObserver _observer; // prevent GC
     private ContentObserver _observer;
+    static final int NOTIFY_ID = 1;
 
 
     @Override
@@ -32,10 +38,17 @@ public class ScreenshotObserverService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String action = intent.getAction();
+        if (action != null) {
+            Log.d(TAG, "onStartCommand: action: " + action);
+        }
+
         Log.d(TAG, "onStartCommand: start watching");
 //        _observer.startWatching();
         getContentResolver().registerContentObserver(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, true, _observer);
+
+        startForeground(NOTIFY_ID, buildNotification());
 
         return START_STICKY;
     }
@@ -137,5 +150,23 @@ public class ScreenshotObserverService extends Service {
 //        intent.putExtra(UploadService.KEY_TITLE, ""); // TODO get foreground app name
 
         startService(intent);
+    }
+
+    private Notification buildNotification() {
+        // tap to open settings activity
+        Intent intent = new Intent(this, SettingsActivity.class);
+        PendingIntent pendingIntent = TaskStackBuilder.create(this)
+                .addNextIntent(intent)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setContentTitle(getText(R.string.title_notification_stay))
+                .setContentText(getText(R.string.desc_notification_stay))
+                .setContentIntent(pendingIntent)
+                // hide icon on notification bar
+                .setPriority(NotificationCompat.PRIORITY_MIN)
+                .build();
     }
 }
